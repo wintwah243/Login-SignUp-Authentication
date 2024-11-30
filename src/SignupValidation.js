30546c589966a3646c5b5965f58b8b6c59aff767
Shi_Shi <142196738+wintwah243@users.noreply.github.com>
2024-11-30 10:37:22 +0630

    Update SignupValidation.js

--- src/SignupValidation.js
+++ src/SignupValidation.js
@@ -11,7 +11,7 @@ function validation(values ) {
         error.name = ''
     }
     if(values.email === ''){
-        error.email = "Name should not be empty"
+        error.email = "Email should not be empty"
     }
     else if(!email_pattern.test(values.email)) {
         error.email = 'Email Didnt match'

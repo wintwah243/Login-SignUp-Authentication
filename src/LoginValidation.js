0b1f7cf50c7d28377e8a3fecd1f73b76cf6b62c0
Shi_Shi <142196738+wintwah243@users.noreply.github.com>
2024-11-30 10:37:40 +0630

    Update LoginValidation.js

--- src/LoginValidation.js
+++ src/LoginValidation.js
@@ -5,7 +5,7 @@ function validation(values ) {
     const password_pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$/
 
     if(values.email === ''){
-        error.email = "Name should not be empty"
+        error.email = "Email should not be empty"
     }
     else if(!email_pattern.test(values.email)) {
         error.email = 'Email Didnt match'
